5c74afae23ad5554a5bcafcf74c2715c18ae3ce9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-31 16:49:01 -0400

    clean up falling entity

--- src/game/falling/FallingEntity.java
+++ src/game/falling/FallingEntity.java
@@ -9,14 +9,13 @@ import java.util.Map;
 
 import static game.chunk.Chunk.placeBlock;
 import static game.collision.Collision.applyInertia;
-import static game.item.ItemDefinition.createItemBlockMeshReturnsMesh;
 
 public class FallingEntity {
     private final static Map<Integer, FallingEntityObject> objects = new HashMap<>();
     private static int currentID = 0;
 
     public static void addFallingEntity(Vector3d pos, Vector3f inertia, int blockID){
-        objects.put(currentID, new FallingEntityObject(pos, inertia, createItemBlockMeshReturnsMesh(blockID), currentID, blockID));
+        objects.put(currentID, new FallingEntityObject(pos, inertia, currentID, blockID));
         currentID++;
     }
 
@@ -25,9 +24,6 @@ public class FallingEntity {
             boolean onGround = applyInertia(thisObject.pos, thisObject.inertia, false, 0.45f, 1f, true, false, true, false, false);
             if (thisObject.inertia.y == 0 || onGround){
                 placeBlock((int)Math.floor(thisObject.pos.x), (int)Math.floor(thisObject.pos.y), (int)Math.floor(thisObject.pos.z), thisObject.ID, 0);
-                if (thisObject.mesh != null){
-                    thisObject.mesh.cleanUp(false);
-                }
                 objects.remove(thisObject.key);
                 return;
             }
--- src/game/falling/FallingEntityObject.java
+++ src/game/falling/FallingEntityObject.java
@@ -1,20 +1,17 @@
 package game.falling;
 
-import engine.graphics.Mesh;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
 
 public class FallingEntityObject {
     public Vector3d pos;
     public Vector3f inertia;
-    public Mesh mesh;
     public int key;
     public int ID;
 
-    public FallingEntityObject(Vector3d pos, Vector3f inertia, Mesh mesh, int key, int ID){
+    public FallingEntityObject(Vector3d pos, Vector3f inertia, int key, int ID){
         this.pos = pos;
         this.inertia = inertia;
-        this.mesh = mesh;
         this.key = key;
         this.ID = ID;
     }
