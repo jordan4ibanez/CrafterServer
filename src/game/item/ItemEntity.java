f2ab96cadcd0772141ae579df917ee9e3dcfb726
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-31 17:26:28 -0400

    Clean up more code

--- src/game/item/ItemEntity.java
+++ src/game/item/ItemEntity.java
@@ -1,19 +1,16 @@
 package game.item;
 
+import game.player.Player;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
-import org.joml.Vector3i;
 
 import java.util.*;
 
 import static engine.FancyMath.getDistance;
 import static engine.Time.getDelta;
-import static engine.sound.SoundAPI.playSound;
-import static game.chunk.Chunk.getLight;
 import static game.collision.Collision.applyInertia;
-import static game.crafting.Inventory.addItemToInventory;
 import static game.item.Item.getCurrentID;
-import static game.player.Player.getPlayerPosWithCollectionHeight;
+import static game.player.Player.getAllPlayers;
 
 public class ItemEntity {
     private final static Map<Integer, Item> items = new HashMap<>();
@@ -55,26 +52,6 @@ public class ItemEntity {
             }
 
             thisItem.timer += delta;
-            thisItem.lightUpdateTimer += delta;
-
-            Vector3i currentFlooredPos = new Vector3i((int)Math.floor(thisItem.pos.x), (int)Math.floor(thisItem.pos.y), (int)Math.floor(thisItem.pos.z));
-
-            //poll local light every half second
-            if (thisItem.lightUpdateTimer >= 0.25f || !currentFlooredPos.equals(thisItem.oldFlooredPos)){
-
-                byte newLight = getLight(currentFlooredPos.x, currentFlooredPos.y, currentFlooredPos.z);
-
-                //don't do extra work if nothing changed
-                if (newLight != thisItem.light){
-                    thisItem.light = newLight;
-                    //System.out.println("rebuild light mesh");
-                    thisItem.rebuildLightMesh(thisItem);
-                }
-
-                thisItem.lightUpdateTimer = 0f;
-            }
-
-            thisItem.oldFlooredPos = currentFlooredPos;
 
             //delete items that are too old
             if (thisItem.timer > 50f){
@@ -83,30 +60,29 @@ public class ItemEntity {
 
             //collect items after 3 seconds
             if (thisItem.timer > 3f){
-                if (getDistance(thisItem.pos, getPlayerPosWithCollectionHeight()) < 3f){
-                    if (!thisItem.collecting){
-                        if (addItemToInventory(thisItem.name)) {
-                            playSound("pickup");
+                for (Object player : getAllPlayers()) {
+                    Player thisPlayer = (Player) player;
+
+                    if (getDistance(thisItem.pos, thisPlayer.pos) < 3f) {
+                        if (!thisItem.collecting) {
                             thisItem.collecting = true;
                             thisItem.collectionTimer = 0.1f;
                         }
-                    }
-                    //do not do else-if here, can go straight to this logic
-                    if (thisItem.collecting) {
-                        Vector3d normalizedPos = new Vector3d(getPlayerPosWithCollectionHeight());
+                        //do not do else-if here, can go straight to this logic
+                        Vector3d normalizedPos = new Vector3d(thisPlayer.pos.add(0,thisPlayer.collectionHeight,0));
                         normalizedPos.sub(thisItem.pos).normalize().mul(15f);
 
                         Vector3f normalizedDirection = new Vector3f();
-                        normalizedDirection.x = (float)normalizedPos.x;
-                        normalizedDirection.y = (float)normalizedPos.y;
-                        normalizedDirection.z = (float)normalizedPos.z;
+                        normalizedDirection.x = (float) normalizedPos.x;
+                        normalizedDirection.y = (float) normalizedPos.y;
+                        normalizedDirection.z = (float) normalizedPos.z;
 
                         thisItem.inertia = normalizedDirection;
                     }
-                }
 
-                if (getDistance(thisItem.pos, getPlayerPosWithCollectionHeight()) < 0.2f || thisItem.deletionOkay){
-                    deletionQueue.add(thisItem.ID);
+                    if (getDistance(thisItem.pos, new Vector3d(thisPlayer.pos.add(0,thisPlayer.collectionHeight,0))) < 0.2f || thisItem.deletionOkay) {
+                        deletionQueue.add(thisItem.ID);
+                    }
                 }
             }
 
@@ -116,25 +92,6 @@ public class ItemEntity {
                 applyInertia(thisItem.pos, thisItem.inertia, false, itemCollisionWidth, itemCollisionWidth, true, false, true, false, false);
             }
 
-            thisItem.rotation.y += delta * 50;
-
-            if (thisItem.rotation.y > 360f) {
-                thisItem.rotation.y -= 360f;
-            }
-
-            if (thisItem.floatUp){
-                thisItem.hover += delta / 10;
-                if (thisItem.hover >= 0.5f){
-                    thisItem.floatUp = false;
-                }
-            } else {
-                thisItem.hover -= delta / 10;
-                if (thisItem.hover <= 0.0f){
-                    thisItem.floatUp = true;
-                }
-            }
-
-
             if (thisItem.pos.y < 0){
                 deletionQueue.add(thisItem.ID);
             }
@@ -142,15 +99,7 @@ public class ItemEntity {
 
         while (!deletionQueue.isEmpty()){
             int thisItemKey = deletionQueue.pop();
-            Item thisItem = items.get(thisItemKey);
-            if (thisItem != null && thisItem.mesh != null){
-                thisItem.mesh.cleanUp(false);
-            }
             items.remove(thisItemKey);
         }
-    }
-
-    public static void clearItems(){
-        items.clear();
     }
 }
--- src/game/item/ItemRegistration.java
+++ src/game/item/ItemRegistration.java
@@ -2,14 +2,10 @@ package game.item;
 
 import org.joml.Vector3d;
 
-import static engine.sound.SoundAPI.playSound;
 import static game.blocks.BlockDefinition.isWalkable;
 import static game.chunk.Chunk.getBlock;
 import static game.chunk.Chunk.setBlock;
 import static game.item.ItemDefinition.registerItem;
-import static game.crafting.Inventory.removeItemFromInventory;
-import static game.player.Player.getCurrentInventorySelection;
-import static game.player.Player.getPlayerDir;
 
 public class ItemRegistration {
 
@@ -42,21 +38,7 @@ public class ItemRegistration {
         }
 
 
-        ItemModifier test = new ItemModifier() {
-            @Override
-            public void onPlace(Vector3d pos) {
-                if (isWalkable(getBlock((int)pos.x,(int)pos.y - 1, (int) pos.z))) {
-                    byte rot = getPlayerDir();
-                    setBlock((int) pos.x, (int) pos.y + 1, (int) pos.z, 23, rot);
-                    setBlock((int) pos.x, (int) pos.y, (int) pos.z, 24, rot);
-                    playSound("wood_1");
-
-                    removeItemFromInventory(getCurrentInventorySelection(), 0);
-                }
-            }
-        };
-
-        registerItem("door", "textures/door.png", test);
+        registerItem("door", "textures/door.png", null);
 
         registerItem("boat", "textures/boatitem.png", null);
 
