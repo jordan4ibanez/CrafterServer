6c9957a352e6e2409ddbdbfb23011660a4dabccf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-10 17:29:33 -0400

    Unhook more client settings

--- src/engine/settings/Settings.java
+++ src/engine/settings/Settings.java
@@ -4,25 +4,7 @@ import static engine.disk.Disk.saveSettingsToDisk;
 
 public class Settings {
 
-    //todo: fix crashing when players mess with Settings.conf
-
     private static SettingsObject settings;
-
-    //false for production (debugInfo)
-    private static boolean debugInfo = false;
-    private static boolean vSync;
-    private static boolean graphicsMode; //true = fancy, false = fast
-    private static byte chunkLoading;
-    private static int renderDistance;
-
-    private static int keyForward;
-    private static int keyBack;
-    private static int keyLeft;
-    private static int keyRight;
-    private static int keySneak;
-    private static int keyDrop;
-    private static int keyJump;
-    private static int keyInventory;
 
     public static void loadSettings(){
 
@@ -34,19 +16,6 @@ public class Settings {
             //instantiate new object
             settings = new SettingsObject();
 
-            vSync = true;
-            settings.vSync = true;
-
-            graphicsMode = true;
-            settings.graphicsMode = true;
-
-            renderDistance = 5;
-            settings.renderDistance = 5;
-
-            //lower slower, higher faster
-            chunkLoading = 2;
-            settings.chunkLoading = 2;
-
             //save default values
             saveSettingsToDisk(settings);
 
@@ -54,109 +23,7 @@ public class Settings {
 
             //dump new settings in
             settings = loadedSettings;
-
-            vSync = settings.vSync;
-            graphicsMode = settings.graphicsMode;
-            renderDistance = settings.renderDistance;
-            chunkLoading = settings.chunkLoading;
-
-            keyForward = settings.keyForward;
-            keyBack = settings.keyBack;
-            keyLeft = settings.keyLeft;
-            keyRight = settings.keyRight;
-            keySneak = settings.keySneak;
-            keyDrop = settings.keyDrop;
-            keyJump = settings.keyJump;
-            keyInventory = settings.keyInventory;
         }
-    }
-
-
-    public static byte getSettingsChunkLoad(){
-        return chunkLoading;
-    }
-
-    //graphics mode
-    public static void setGraphicsMode(boolean newGraphicsMode){
-        graphicsMode = newGraphicsMode;
-        settings.graphicsMode = newGraphicsMode;
-    }
-    public static boolean getGraphicsMode(){
-        return graphicsMode;
-    }
-
-    //input keys
-
-    //forward
-    public static void setKeyForward(int newKey){
-        keyForward = newKey;
-        settings.keyForward = newKey;
-    }
-    public static int getKeyForward(){
-        return keyForward;
-    }
-
-    //back
-    public static void setKeyBack(int newKey){
-        keyBack = newKey;
-        settings.keyBack = newKey;
-    }
-    public static int getKeyBack(){
-        return keyBack;
-    }
-
-    //left
-    public static void setKeyLeft(int newKey){
-        keyLeft = newKey;
-        settings.keyLeft = newKey;
-    }
-    public static int getKeyLeft(){
-        return keyLeft;
-    }
-
-    //right
-    public static void setKeyRight(int newKey){
-        keyRight = newKey;
-        settings.keyRight = newKey;
-    }
-    public static int getKeyRight(){
-        return keyRight;
-    }
-
-    //sneak
-    public static void setKeySneak(int newKey){
-        keySneak = newKey;
-        settings.keySneak = newKey;
-    }
-    public static int getKeySneak(){
-        return keySneak;
-    }
-
-    //drop
-    public static void setKeyDrop(int newKey){
-        keyDrop = newKey;
-        settings.keyDrop = newKey;
-    }
-    public static int getKeyDrop(){
-        return keyDrop;
-    }
-
-    //jump
-    public static void setKeyJump(int newKey){
-        keyJump = newKey;
-        settings.keyJump = newKey;
-    }
-    public static int getKeyJump(){
-        return keyJump;
-    }
-
-    //inventory
-    public static void setKeyInventory(int newKey){
-        keyInventory = newKey;
-        settings.keyInventory = newKey;
-    }
-    public static int getKeyInventory(){
-        return keyInventory;
     }
 
     //settings saving
