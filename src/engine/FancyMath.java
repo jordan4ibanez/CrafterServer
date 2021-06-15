6c6be3f1b636de1fcab7262dc2f311f5e1cf759f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-14 20:48:13 -0400

    Add 2d distance to fancy math

--- src/engine/FancyMath.java
+++ src/engine/FancyMath.java
@@ -1,5 +1,6 @@
 package engine;
 
+import org.joml.Vector2d;
 import org.joml.Vector3d;
 import java.util.Random;
 
@@ -25,6 +26,15 @@ public class FancyMath {
 
     public static double getDistance(double x1, double y1, double z1, double x2, double y2, double z2){
         return Math.hypot((x1 - x2), Math.hypot((y1 - y2),(z1 - z2)));
+    }
+
+
+    public static double getDistance2D(double x1, double z1, double x2, double z2){
+        return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(z1-z2, 2));
+    }
+
+    public static double getDistance2D(Vector2d pos1, Vector2d pos2){
+        return Math.sqrt(Math.pow(pos1.x-pos2.x, 2) + Math.pow(pos1.y-pos2.y, 2));
     }
 
 }
